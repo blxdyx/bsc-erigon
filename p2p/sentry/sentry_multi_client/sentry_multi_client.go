4ea698eeb9c8da46541b607df0817f3aab76facf
blxdyx <blxdyx@outlook.com>
2024-08-01 11:51:07 +0800

    edit config

--- p2p/sentry/sentry_multi_client/sentry_multi_client.go
+++ p2p/sentry/sentry_multi_client/sentry_multi_client.go
@@ -562,7 +562,7 @@ func (cs *MultiClient) newBlock66(ctx context.Context, inreq *proto_sentry.Inbou
 		return fmt.Errorf("newBlock66: %w, peerId: %x", err, fmt.Sprintf("%x", sentry.ConvertH512ToPeerID(inreq.PeerId))[:8])
 	}
 	if err := request.Block.HashCheck(); err != nil {
-		return fmt.Errorf("newBlock66: %w", err)
+		return fmt.Errorf("newBlock66: %w, peerId: %x", err, fmt.Sprintf("%x", sentry.ConvertH512ToPeerID(inreq.PeerId))[:8])
 	}
 
 	if request.Sidecars != nil && len(request.Sidecars) > 0 {
