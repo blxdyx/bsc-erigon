31259cab9981fd64e94a2d5ec29c1e993e85c7f1
blxdyx <blxdyx@outlook.com>
2024-11-07 15:26:23 +0800

    add logs

--- core/state/txtask.go
+++ core/state/txtask.go
@@ -391,7 +391,9 @@ func (q *ResultsQueueIter) Close() {
 	q.q.Unlock()
 }
 func (q *ResultsQueueIter) HasNext(outputTxNum uint64) bool {
-	log.Info("Log tx", "outputTxNum", outputTxNum, "rwsIt", (*q.results)[0].TxNum)
+	if len(*q.results) > 0 {
+		log.Info("Log tx", "outputTxNum", outputTxNum, "rwsIt", (*q.results)[0].TxNum)
+	}
 	return len(*q.results) > 0 && (*q.results)[0].TxNum == outputTxNum
 }
 func (q *ResultsQueueIter) PopNext() *TxTask {
