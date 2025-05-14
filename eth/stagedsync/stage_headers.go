d9017fe45cf8dfab84808c53426e1fd6d95d4937
blxdyx <blxdyx@users.noreply.github.com>
2025-05-14 11:00:06 +0800

    stage_headers.go: optimize request time

--- eth/stagedsync/stage_headers.go
+++ eth/stagedsync/stage_headers.go
@@ -237,7 +237,7 @@ Loop:
 		}
 
 		// Send skeleton request if required
-		if time.Since(lastSkeletonTime) > 1500*time.Millisecond {
+		if time.Since(lastSkeletonTime) > 3*time.Millisecond {
 			req = cfg.hd.RequestSkeleton()
 			if req != nil {
 				peer, sentToPeer = cfg.headerReqSend(ctx, req)
