77f760aa213ac9574f1b1603f4bc10545d785611
blxdyx <blxdyx@users.noreply.github.com>
2025-05-30 14:27:54 +0800

    upstream: update v304

--- erigon-lib/chain/params/protocol.go
+++ erigon-lib/chain/params/protocol.go
@@ -207,7 +207,7 @@ const (
 	MinBlocksForBlobRequests           uint64 = uint64(float64(MinTimeDurationForBlobRequests) / 0.75) // ref: https://github.com/bnb-chain/BEPs/blob/master/BEPs/BEP-524.md#421-change-table.
 	DefaultExtraReserveForBlobRequests uint64 = uint64(24 * 3600 / 0.75)                               // it adds more time for expired blobs for some request cases, like expiry blob when remote peer is syncing, default 1 day.
 
-	BreatheBlockInterval uint64 = 24 * 3600 // Controls the interval for updateValidatorSetV2
+	BreatheBlockInterval uint64 = 600 // Controls the interval for updateValidatorSetV2
 
 	// used for testing:
 	//     [1,9] except 2 --> used as turn length directly
