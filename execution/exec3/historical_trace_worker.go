10b496534224ed05c49e6d7a4eeccd7d9653477f
blxdyx <blxdyx@users.noreply.github.com>
2025-06-10 10:35:32 +0800

    stage_execute.go: more aggresive

--- execution/exec3/historical_trace_worker.go
+++ execution/exec3/historical_trace_worker.go
@@ -419,7 +419,7 @@ func processResultQueueHistorical(consumer TraceConsumer, rws *state.ResultsQueu
 			applyWorker.RunTxTaskNoLock(txTask.Reset())
 		}
 		if txTask.Error != nil {
-			log.Info("processResultQueueHistorical", "tx.Index", txTask.TxIndex, "txTask.BlockNum", "err", txTask.Error)
+			log.Info("processResultQueueHistorical", "tx.Index", txTask.TxIndex, "txTask.BlockNum", txTask.BlockNum, "err", txTask.Error)
 			return outputTxNum, false, txTask.Error
 		}
 
