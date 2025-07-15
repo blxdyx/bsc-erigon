6b6d950e30a79cf536b8513e966fbcbf2f30340b
blxdyx <blxdyx@users.noreply.github.com>
2025-07-15 15:39:38 +0800

    block_snapshots.go: revert update BlockCompressCfg

--- turbo/snapshotsync/freezeblocks/block_snapshots.go
+++ turbo/snapshotsync/freezeblocks/block_snapshots.go
@@ -591,10 +591,10 @@ var BlockCompressCfg = seg.Cfg{
 	MinPatternScore: 1_000,
 	MinPatternLen:   8, // `5` - reducing ratio because producing too much prefixes
 	MaxPatternLen:   128,
-	SamplingFactor:  1,         // `4` - to save time, '1' - to save disk space
-	MaxDictPatterns: 64 * 1024, // the lower RAM used by huffman tree (arrays)
+	SamplingFactor:  4,         // `4` - to save time, '1' - to save disk space
+	MaxDictPatterns: 16 * 1024, // the lower RAM used by huffman tree (arrays)
 
-	DictReducerSoftLimit: 2_000_000,
+	DictReducerSoftLimit: 1_000_000,
 	Workers:              1,
 }
 
