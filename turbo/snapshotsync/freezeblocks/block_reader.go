97140c0c37fc3d9ce6ef126cf3f3fb2914931e93
blxdyx <blxdyx@users.noreply.github.com>
2025-07-21 11:27:12 +0800

    block_reader.go: add logs

--- turbo/snapshotsync/freezeblocks/block_reader.go
+++ turbo/snapshotsync/freezeblocks/block_reader.go
@@ -1637,7 +1637,7 @@ func (r *BlockReader) ReadBlobByNumber(ctx context.Context, tx kv.Getter, blockH
 		}
 		return r.bs.ReadBlobSidecars(ctx, blockHeight, blockHash)
 	}
-	//log.Info("Read BlobSidecar from snapshots")
+	log.Info("Read BlobSidecar from snapshots", "blockHeight", blockHeight, "maxBlobInFiles", maxBlobInFiles)
 	blobs, err := r.bscSn.ReadBlobSidecars(blockHeight)
 	if err != nil {
 		return nil, false, err
